9d632dbe3f8a913bf1fadd37b84d39f2ad325d35
Conrad Indiono <indygemma@gmail.com>
2017-05-19 16:27:21 +0200

    add license info for frontend code

--- frontend/project.clj
+++ frontend/project.clj
@@ -1,3 +1,17 @@
+; Copyright (c) 2017 Conrad Indiono
+;
+; This program is free software: you can redistribute it and/or modify it under
+; the terms of the GNU General Public License as published by the Free Software
+; Foundation, either version 3 of the License, or (at your option) any later
+; version.
+;
+; This program is distributed in the hope that it will be useful, but WITHOUT ANY
+; WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+; PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License along with
+; this program (see file COPYING). If not, see <http://www.gnu.org/licenses/>.
+
 (defproject btw-client "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.229"]
